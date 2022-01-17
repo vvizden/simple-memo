6c4dfd74167cdee5cc8171cf86d1780ed7f8b3df
viz <820853182@qq.com>
2022-01-17 20:01:19 +0800

    feat: add CRUD

--- apps/app-api/src/service/app-service.ts
+++ apps/app-api/src/service/app-service.ts
@@ -21,4 +21,13 @@ export type AppVOParams = Partial<Omit<AppVO, 'id'>> | undefined
 export interface AppService extends BaseService<'AppService'> {
   list(params?: AppVOParams): AppVO[]
   listWithItems(params?: AppVOParams): AppVOWithItems[]
+  add(vo: Omit<AppVO, 'id'> & { items?: Omit<AppItemVO, 'id'>[] }): false | AppVOWithItems
+  update(
+    vo: AppVO & {
+      itemsRemove?: bigint[]
+      itemsUpdate?: Omit<AppItemVO, 'app_id'>[]
+      itemsAdd?: Omit<AppItemVO, 'id' | 'app_id'>[]
+    },
+  ): boolean
+  remove(id: bigint): boolean
 }
--- apps/main/src/service/app-service.ts
+++ apps/main/src/service/app-service.ts
@@ -1,4 +1,4 @@
-import type { AppService, AppVOParams } from 'app-api'
+import type { AppService, AppVOParams, AppVO, AppItemVO } from 'app-api'
 import type { IpcService } from 'electron-ipc-main'
 import type { DBClientService } from './db-client-service'
 import type { PrismaClient } from '@prisma/client'
@@ -42,5 +42,122 @@ export class AppServiceImpl implements IpcService<AppService> {
       },
     })
     return apps
+  }
+
+  async add(vo: Omit<AppVO, 'id'> & { items?: Omit<AppItemVO, 'id'>[] }) {
+    const client = await this.dbClientService.getClient()
+    if (!client) {
+      return false
+    }
+
+    const app = await client.memo_app.create({
+      include: {
+        items: true,
+      },
+      data: {
+        name: vo.name,
+        items: {
+          create: vo.items,
+        },
+      },
+    })
+
+    return app
+  }
+
+  async update(
+    vo: AppVO & {
+      itemsRemove?: bigint[]
+      itemsUpdate?: Omit<AppItemVO, 'app_id'>[]
+      itemsAdd?: Omit<AppItemVO, 'id' | 'app_id'>[]
+    },
+  ) {
+    const client = await this.dbClientService.getClient()
+    if (!client) {
+      return false
+    }
+
+    const transactions = []
+
+    if (vo.itemsRemove) {
+      const removeItems = client.memo_app_item.deleteMany({
+        where: {
+          app_id: vo.id,
+        },
+      })
+      transactions.push(removeItems)
+    }
+
+    if (vo.itemsUpdate) {
+      const updateItems = vo.itemsUpdate.map(({ id, title, value }) => {
+        return client.memo_app_item.updateMany({
+          where: {
+            app_id: vo.id,
+            id,
+          },
+          data: {
+            title,
+            value,
+          },
+        })
+      })
+      transactions.push(...updateItems)
+    }
+
+    if (vo.itemsAdd) {
+      const addItems = vo.itemsAdd.map(({ title, value }) => {
+        return client.memo_app_item.create({
+          data: {
+            app_id: vo.id,
+            title,
+            value,
+          },
+        })
+      })
+      transactions.push(...addItems)
+    }
+
+    transactions.push(
+      client.memo_app.update({
+        where: {
+          id: vo.id,
+        },
+        data: {
+          name: vo.name,
+        },
+      }),
+    )
+
+    try {
+      await client.$transaction(transactions)
+      return true
+    } catch (error) {
+      return false
+    }
+  }
+
+  async remove(id: bigint) {
+    const client = await this.dbClientService.getClient()
+    if (!client) {
+      return false
+    }
+
+    try {
+      await client.$transaction([
+        client.memo_app_item.deleteMany({
+          where: {
+            app_id: id,
+          },
+        }),
+        client.memo_app.delete({
+          where: {
+            id,
+          },
+        }),
+      ])
+      return true
+    } catch (error) {
+      return false
+    }
   }
 }
